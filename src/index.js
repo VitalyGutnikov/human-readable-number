ff9b7d8bc01418578ea1070203ef81efe16c7130
Виталий Гутников <vitaly_vg.st_prod@mail.ru>
2022-08-05 20:55:22 +0300

    complete test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function toReadable (number) {
-    number = number.toString();
+  number = number.toString();
 	let result = '';
 	let toNine = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fiveteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     let twentyToNinety = ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine'];
     let dozens;
   if (number < 10) {
@@ -17,11 +17,7 @@ module.exports = function toReadable (number) {
     } else if(number >= 100 && number < 1000){
       let hundred = Math.floor(number / 100);
       
-      result = `${toNine[hundred]} hundred 
-${dozens = number[1] == 0 && number[2] == 0 ? result :
-      (number[1] == 0  ? toNine[number[2]] : 
-      (number[1] > 0 && number[1] < 2 ? tenToNineteen[number[2]] : 
-       (number[1] > 1 && number[2] > 1 ? twentyToNinety[number[1] - 2] + 'ty' + ' ' + toNine[number[2]] : (number[1] > 1 &&  number[2] == 0 ? twentyToNinety[number[1] - 2] + 'ty' + result : ''))))}`
+      result = `${toNine[hundred]} hundred ${dozens = number[1] == 0 && number[2] == 0 ? result : (number[1] == 0  ? toNine[number[2]] : (number[1] > 0 && number[1] < 2 ? tenToNineteen[number[2]] : (number[1] > 1 && number[2] >= 1 ? twentyToNinety[number[1] - 2] + 'ty' + ' ' + toNine[number[2]] : (number[1] > 1 &&  number[2] == 0 ? twentyToNinety[number[1] - 2] + 'ty' + result : ''))))}`
       
       result = result.trim();
     } else {
