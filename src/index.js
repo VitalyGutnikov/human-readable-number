403ea4c26c90348bc4737f4e5bc45f4d0d41163c
Виталий Гутников <vitaly_vg.st_prod@mail.ru>
2022-08-05 21:01:49 +0300

    complete test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function toReadable (number) {
 	let result = '';
 	let toNine = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
     let tenToNineteen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    let twentyToNinety = ['twen', 'thir', 'for', 'fif', 'six', 'seven', 'eigh', 'nine'];
+    let twentyToNinety = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let dozens;
   if (number < 10) {
       result = `${result}${toNine[number]}`;
@@ -11,13 +11,13 @@ module.exports = function toReadable (number) {
       result = tenToNineteen[number[1]];
       
     } else if(number >=20 && number < 100){
-      result = `${twentyToNinety[Math.floor((number / 100) * 10) - 2]}ty ${toNine = number[1] > 0 ? toNine[number[1]]: ''}`;
+      result = `${twentyToNinety[Math.floor((number / 100) * 10) - 2]} ${toNine = number[1] > 0 ? toNine[number[1]]: ''}`;
       result = result.trim();
       
     } else if(number >= 100 && number < 1000){
       let hundred = Math.floor(number / 100);
       
-      result = `${toNine[hundred]} hundred ${dozens = number[1] == 0 && number[2] == 0 ? result : (number[1] == 0  ? toNine[number[2]] : (number[1] > 0 && number[1] < 2 ? tenToNineteen[number[2]] : (number[1] > 1 && number[2] >= 1 ? twentyToNinety[number[1] - 2] + 'ty' + ' ' + toNine[number[2]] : (number[1] > 1 &&  number[2] == 0 ? twentyToNinety[number[1] - 2] + 'ty' + result : ''))))}`
+      result = `${toNine[hundred]} hundred ${dozens = number[1] == 0 && number[2] == 0 ? result : (number[1] == 0  ? toNine[number[2]] : (number[1] > 0 && number[1] < 2 ? tenToNineteen[number[2]] : (number[1] > 1 && number[2] >= 1 ? twentyToNinety[number[1] - 2] + ' ' + toNine[number[2]] : (number[1] > 1 &&  number[2] == 0 ? twentyToNinety[number[1] - 2] + result : ''))))}`
       
       result = result.trim();
     } else {
